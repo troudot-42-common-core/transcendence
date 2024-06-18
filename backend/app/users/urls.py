3a8479ea57e68e9d80b31cf1114d3746c437e80b
0x21x <troudot.42@gmail.com>
2024-06-18 17:47:32 +0200

    CRUD api fully implemented

--- backend/app/users/urls.py
+++ backend/app/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from .views import create, read
+from .views import AuthAPIView
+# from .views import create, read
 
 
 # Routers provide an easy way of automatically determining the URL conf.
-# router = routers.DefaultRouter()
-# router.register(r'users', UserViewSet)
+
 urlpatterns = [
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-    path('create/', create, name="create"),
-    path('read/<int:pk>', read, name="read"),
+    path('', AuthAPIView.as_view()),
+    path('<int:id>', AuthAPIView.as_view()),
+    # path('create/', create, name="create"),
+    # path('read/<int:pk>', read, name="read"),
 ]
--- backend/app/users/views.py
+++ backend/app/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+# from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from annoying.functions import get_object_or_None
@@ -7,30 +8,50 @@ from .models import Users
 # from django.contrib.auth.models import User
 from .serlializers import UserSerializer
 
+class AuthAPIView(APIView):
+    def get(self, request, id, *args, **kwargs):
+        obj = get_object_or_None(Users, id=id)
+        if obj is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user = UserSerializer(obj)
+        return Response(user.data, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def create(request):
-    user = UserSerializer(data=request.data)
-    if not user.is_valid():
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    if Users.objects.filter(username=request.data['username']).exists():
-        return Response({"Username already exists"}, status=status.HTTP_409_CONFLICT)
-    user.validated_data['password'] = make_password('password')
-    user.save()
-    return Response(user.data, status=status.HTTP_201_CREATED)
+    def post(self, request, *args, **kwargs):
+        user = UserSerializer(data=request.data)
+        if not user.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        if Users.objects.filter(username=request.data['username']).exists():
+            return Response({"Username already exists"}, status=status.HTTP_409_CONFLICT)
+        user.validated_data['password'] = make_password('password')
+        user.save()
+        return Response(user.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
-def read(request, pk):
-    obj = get_object_or_None(Users, id=pk)
-    if obj is None:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    user = UserSerializer(obj)
-    return Response(user.data, status=status.HTTP_200_OK)
+    def put(self, request, id, *args, **kwargs):
+        obj = get_object_or_None(Users, id=id)
+        if obj is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        user = UserSerializer(obj, request.data, partial=True)
+        if not user.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user.validated_data['password'] = make_password('password')
+        user.save()
 
-@api_view(['PUT'])
-def update(request):
-    pass
+        return Response(status=status.HTTP_200_OK)
+    # fix UPDATE
 
-@api_view(['DELETE'])
-def delete(request):
-    pass
+    def delete(self, request, id, *args, **kwargs):
+        obj = get_object_or_None(Users, id=id)
+        if obj is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# @api_view(['PUT'])
+# def update(request):
+#     pass
+#
+# @api_view(['DELETE'])
+# def delete(request):
+#     pass
