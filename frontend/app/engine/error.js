1e6da8863538e2410d80303f83e3f32f3800f37b
21x <114038240+0x21x@users.noreply.github.com>
2024-10-04 16:42:33 +0200

    add icon handler on error message (#86)

--- frontend/app/engine/error.js
+++ frontend/app/engine/error.js
@@ -26,17 +26,48 @@ export const addErrorContainer = (div) => {
     const errorContainer = document.createElement('div');
     errorContainer.classList.add('container-xl', 'container-error');
     errorContainer.id = 'errorContainer';
+    errorContainer.innerHTML = `
+        <svg xmlns = "http://www.w3.org/2000/svg" style = "display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+            </symbol>
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </symbol>
+        </svg>
+    `;
     div.insertBefore(errorContainer, div.children[0]);
 };
 
-export const error = (errorString, errorType) => {
+const errorTypes = [
+    {'name': 'primary', 'icon': 'info'},
+    {'name': 'secondary', 'icon': 'info'},
+    {'name': 'success', 'icon': 'success'},
+    {'name': 'danger', 'icon': 'warning'},
+    {'name': 'warning', 'icon': 'warning'},
+    {'name': 'info', 'icon': 'info'},
+    {'name': 'light', 'icon': 'info'},
+    {'name': 'dark', 'icon': 'info'}
+];
+
+export const error = (errorString, errorType, icon='') => {
     const language = localStorage.getItem('language') || 'en';
 
     const errorContainer = document.getElementById('errorContainer');
     if (!errorContainer)
         return;
-    if (!['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'].includes(errorType)) {
+    if (!errorTypes.some((type) => type.name === errorType)) {
         return;
+    }
+
+    let errorIcon = '';
+    if (icon !== '' && errorTypes.some((type) => type.icon === icon)) {
+        errorIcon = icon;
+    } else {
+        errorIcon = errorTypes.find((type) => type.name === errorType).icon;
     }
 
     if (errorContainer.children.length >= 3) {
@@ -47,7 +78,18 @@ export const error = (errorString, errorType) => {
     const error = document.createElement('div');
     error.classList.add('alert', 'alert-' + errorType);
     error.role = 'alert';
-    error.innerHTML = getLanguageDict(language, errorString, errorDict);
+    switch (errorIcon) {
+        case 'success':
+            error.innerHTML = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg';
+            break;
+        case 'warning':
+            error.innerHTML = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg';
+            break;
+        case 'info':
+            error.innerHTML = '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Info:"><use xlink:href="#info-fill"/></svg';
+            break;
+    }
+    error.innerHTML += getLanguageDict(language, errorString, errorDict);
     errorContainer.appendChild(error);
     error.timeout = setTimeout(() => {
         errorContainer.removeChild(error);
