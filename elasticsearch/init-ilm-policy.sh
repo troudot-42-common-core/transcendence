973da525777029f75257e61c31aa3de89a5a8c0a
21x <114038240+0x21x@users.noreply.github.com>
2024-12-16 16:11:55 +0100

    105 stack elk (#108)
    * init of the stack elf
    
    * the stack ELK is now only accesible by kibana on https and only from the host machine
    
    * update readme.md
    
    * block kibana to host machine only
    
    * block connection to kibana to host machine
    
    * add ILM life cycle of indexes
    
    * now elk really works
    
    ---------
    
    Co-authored-by: Timothe Roudot <troudot@k0r4p13.42mulhouse.fr>

--- elasticsearch/init-ilm-policy.sh
+++ elasticsearch/init-ilm-policy.sh
@@ -48,3 +48,54 @@ curl -X PUT "http://elasticsearch:9200/%3Cft_transcendence-%7Bnow%2Fd%7D-000001%
   }
 }
 '
+
+#!/bin/bash
+
+until curl -s http://elasticsearch:9200 >/dev/null; do
+    sleep 1
+done
+
+curl -X PUT "http://elasticsearch:9200/_ilm/policy/ft_transcendence_policy" -H 'Content-Type: application/json' -d'
+{
+  "policy": {
+    "phases": {
+      "hot": {
+        "actions": {
+          "rollover": {
+            "max_size": "5GB",
+            "max_age": "1d"
+          }
+        }
+      },
+      "delete": {
+        "min_age": "30d",
+        "actions": {
+          "delete": {}
+        }
+      }
+    }
+  }
+}
+'
+
+curl -X PUT "http://elasticsearch:9200/_template/ft_transcendence_template" -H 'Content-Type: application/json' -d'
+{
+  "index_patterns": ["ft_transcendence-*"],
+  "settings": {
+    "number_of_shards": 1,
+    "number_of_replicas": 1,
+    "index.lifecycle.name": "ft_transcendence_policy",
+    "index.lifecycle.rollover_alias": "ft_transcendence"
+  }
+}
+'
+
+curl -X PUT "http://elasticsearch:9200/%3Cft_transcendence-%7Bnow%2Fd%7D-000001%3E" -H 'Content-Type: application/json' -d'
+{
+  "aliases": {
+    "ft_transcendence": {
+      "is_write_index": true
+    }
+  }
+}
+'
